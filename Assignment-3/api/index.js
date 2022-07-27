65e85c3e5327919da463927c022cd466802969d4
jwright303 <41707123+jwright303@users.noreply.github.com>
2022-07-26 17:22:07 -0700

    Add files via upload

---
+++ Assignment-3/api/index.js
@@ -0,0 +1,8 @@
+const router = module.exports = require('express').Router();
+
+router.use('/businesses', require('./businesses').router);
+router.use('/reviews', require('./reviews').router);
+router.use('/photos', require('./photos').router);
+router.use('/users', require('./users').router);
+
+module.exports = router
